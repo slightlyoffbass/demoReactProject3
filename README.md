12356862c51865cf8d4ec0a1bf5e080736f8281d
jeff.smith <jeff.smith@usaa.com>
2019-08-28 15:14:29 -0500

    added axios request to unsplashed

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # This app is an exercise of an online react/redux course
 
 ## create project
-npx create-react-app <name of app>
+npx create-react-app nameofapp
+
+## connect to unsplash for car images
+allowed 50 requests per hour
+
+## ajax client options
+- axios - used in this project, lower level
+-- npm install --save axios
+
+- fetch, not a seperate function, built into browsers, method, could cause more code
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
 
     onSearchSubmit(term){
-        console.log(term);
+        Axios.get('https://api.unsplash.com/search/photos',{
+            params:{
+                query: term
+            },
+            headers:{
+                Authorization:'Client-ID 607165ea70b24ba32cbe94cd8e64a4ceb7bfc20084d43783d464415d44527989'
+            }
+        })
     }
     
     render(){
