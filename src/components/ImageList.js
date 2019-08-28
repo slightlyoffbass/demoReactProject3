ec2073d40fcafb4ed3c1ebb53a04550241ae3ee6
jeff.smith <jeff.smith@usaa.com>
2019-08-28 13:18:23 -0500

    basic project structure created

---
+++ src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// class ImageList extends React.Component(){
+    
+//     render(
+//         return(
+//             <div>ImageList</div>
+//         );
+//     )
+// }
