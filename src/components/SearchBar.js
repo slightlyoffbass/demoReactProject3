933b95e5f560ddf2647721edf118dfb64c6ac870
jeff.smith <jeff.smith@usaa.com>
2019-08-28 14:29:51 -0500

    added arrow fucntion, this.state.term can be called without cannot read property of state error

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,15 @@ class SearchBar extends React.Component{
     state = {term:''}
 
     // prevents default submit when enter key is pressed
-    onFormSubmit(event){
+    // by using an error fucntion, this.state.term can be called without cannot read property of state error
+    onFormSubmit = (event) => {
         event.preventDefault();
     }
 
     render(){
         return (
+        // this keyword is a reference back to the class itself
+        // Rule: when finding value of this, look at where the function is called
         <div onSubmit={this.onFormSubmit} className="ui segment">
             <form className="ui form">
                 <label>Image Search</label>
@@ -19,7 +22,7 @@ class SearchBar extends React.Component{
                     className="field" 
                     value={this.state.term}
                     placeholder="enter text"
-                    onChange={(e)=> this.setState({term:e.target.value})}
+                    onChange={(e)=> this.setState({term: e.target.value})}
                 />
             </form>
         </div>
