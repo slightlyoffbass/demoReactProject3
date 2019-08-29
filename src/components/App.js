5f8ad4bca2ad402b493f48047e9ae0c8675304fb
jeff.smith <jeff.smith@usaa.com>
2019-08-29 08:13:30 -0500

    fixed export error

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import Axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
     
     state = {images: []};
-    
-    // Converted to arrow function to prevent the following error
-    // Unhandled Rejection (TypeError): this.setState is not a function
+
     onSearchSubmit = async term =>{
-        const response = await Axios.get('https://api.unsplash.com/search/photos',{
-            params:{
-                query: term
-            },
-            headers:{
-                Authorization:'Client-ID 607165ea70b24ba32cbe94cd8e64a4ceb7bfc20084d43783d464415d44527989'
-            }
+        const response = await unsplash.get('/search/photos',{
+            params:{ query: term }
         });
         
         this.setState({images: response.data.results })
