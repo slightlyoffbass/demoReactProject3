74457a87a0d9f0b9a87dba99e780b2388ae95cb6
jeff.smith <jeff.smith@usaa.com>
2019-08-29 08:29:53 -0500

    add map for images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-// class ImageList extends React.Component(){
-    
-//     render(
-//         return(
-//             <div>ImageList</div>
-//         );
-//     )
-// }
+const ImageList =(props)=>{
+        
+    const images = props.images.map((image)=>{
+        return <img src={image.urls.regular}/>
+    });
+
+    return(
+        <div>{images}</div>
+    );
+}
+export default ImageList;
